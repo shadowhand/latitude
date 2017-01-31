02959d793a2284de828e15785b0b7e7010164fef
Woody Gilk <woody.gilk@gmail.com>
2017-01-31 16:08:49 -0600

    Release 0.1.3 for PHP 5.x

--- src/SqlServer/LikeValue.php
+++ src/SqlServer/LikeValue.php
@@ -1,20 +1,16 @@
 <?php
-declare(strict_types=1);
 
 namespace Latitude\QueryBuilder\SqlServer;
 
 use Latitude\QueryBuilder\LikeValue as Base;
-
 abstract class LikeValue extends Base
 {
-    public static function escape(string $value): string
+    public static function escape($value)
     {
         $value = parent::escape($value);
-
         // MSSQL also includes character ranges.
         $value = str_replace('[', '\\[', $value);
         $value = str_replace(']', '\\]', $value);
-
         return $value;
     }
 }
