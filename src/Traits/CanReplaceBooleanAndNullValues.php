c27b4e6a6bbce451b71782bfb396e4771b30d0bc
Woody Gilk <woody.gilk@gmail.com>
2017-02-01 14:06:31 -0600

    Release 0.2.0 for PHP 5.x

--- src/Traits/CanReplaceBooleanAndNullValues.php
+++ src/Traits/CanReplaceBooleanAndNullValues.php
@@ -1,10 +1,8 @@
 <?php
-declare(strict_types=1);
 
 namespace Latitude\QueryBuilder\Traits;
 
 use Latitude\QueryBuilder\Expression;
-
 trait CanReplaceBooleanAndNullValues
 {
     /**
@@ -14,30 +12,25 @@ trait CanReplaceBooleanAndNullValues
      * This will fail for null and boolean values. By replacing the values
      * directly more consistent queries can be built.
      */
-    protected function placeholderValue(int $index): string
+    protected function placeholderValue($index)
     {
         $value = $this->params[$index];
-
         if ($value === true) {
             unset($this->params[$index]);
             return 'TRUE';
         }
-
         if ($value === false) {
             unset($this->params[$index]);
             return 'FALSE';
         }
-
         if ($value === null) {
             unset($this->params[$index]);
             return 'NULL';
         }
-
         if ($value instanceof Expression) {
             unset($this->params[$index]);
             return $value->sql();
         }
-
         return '?';
     }
 }
--- tests/Postgres/UpdateQueryTest.php
+++ tests/Postgres/UpdateQueryTest.php
@@ -1,57 +1,25 @@
 <?php
-declare(strict_types=1);
 
 namespace Latitude\QueryBuilder\Postgres;
 
 use Latitude\QueryBuilder\Conditions;
 use PHPUnit_Framework_TestCase as TestCase;
-
 class UpdateQueryTest extends TestCase
 {
     public function testUpdate()
     {
         $table = 'users';
-        $map = [
-            'username' => 'mr-smith',
-        ];
-
-        $update = UpdateQuery::make($table, $map)
-            ->where(
-                Conditions::make('username = ?', 'jsmith')
-            )
-            ->returning(['updated_at']);
-
-        $this->assertSame(
-            'UPDATE users SET username = ? WHERE username = ? RETURNING updated_at',
-            $update->sql()
-        );
-
-        $this->assertSame(
-            ['mr-smith', 'jsmith'],
-            $update->params()
-        );
+        $map = ['username' => 'mr-smith'];
+        $update = UpdateQuery::make($table, $map)->where(Conditions::make('username = ?', 'jsmith'))->returning(['updated_at']);
+        $this->assertSame('UPDATE users SET username = ? WHERE username = ? RETURNING updated_at', $update->sql());
+        $this->assertSame(['mr-smith', 'jsmith'], $update->params());
     }
-
     public function testUpdateWithoutReturning()
     {
         $table = 'users';
-        $map = [
-            'username' => 'mr-smith',
-        ];
-
-        $update = UpdateQuery::make($table, $map)
-            ->where(
-                Conditions::make('username = ?', 'jsmith')
-            );
-
-        $this->assertSame(
-            'UPDATE users SET username = ? WHERE username = ?',
-            $update->sql()
-        );
-
-        $this->assertSame(
-            ['mr-smith', 'jsmith'],
-            $update->params()
-        );
+        $map = ['username' => 'mr-smith'];
+        $update = UpdateQuery::make($table, $map)->where(Conditions::make('username = ?', 'jsmith'));
+        $this->assertSame('UPDATE users SET username = ? WHERE username = ?', $update->sql());
+        $this->assertSame(['mr-smith', 'jsmith'], $update->params());
     }
 }
