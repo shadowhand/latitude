5fbdc50c2727487b8e6dd7ead4c9d05752482204
Woody Gilk <woody.gilk@gmail.com>
2017-02-02 09:01:26 -0600

    Release 0.2.1 for PHP 5.x

--- src/Traits/CanReplaceBooleanAndNullValues.php
+++ src/Traits/CanReplaceBooleanAndNullValues.php
@@ -1,10 +1,8 @@
 <?php
-declare(strict_types=1);
 
 namespace Latitude\QueryBuilder\Traits;
 
 use Latitude\QueryBuilder\Expression;
-
 trait CanReplaceBooleanAndNullValues
 {
     /**
@@ -14,42 +12,35 @@ trait CanReplaceBooleanAndNullValues
      * This will fail for null and boolean values. By replacing the values
      * directly more consistent queries can be built.
      */
-    protected function placeholderValue(int $index): string
+    protected function placeholderValue($index)
     {
         $value = $this->params[$index];
-
         if ($this->isPlaceholderValue($value)) {
             return '?';
         }
-
         if ($value instanceof Expression) {
             return $value->sql();
         }
-
         // null -> "NULL", true -> "TRUE", etc
         return \strtoupper(\var_export($value, true));
     }
-
     /**
      * Determine if a value can be represented by a placeholder.
      */
-    protected function isPlaceholderValue($value): bool
+    protected function isPlaceholderValue($value)
     {
         if (\in_array($value, [true, false, null], true)) {
             return false;
         }
-
         if ($value instanceof Expression) {
             return false;
         }
-
         return true;
     }
-
     /**
      * Get all parameters that can be placeholders.
      */
-    protected function placeholderParams(): array
+    protected function placeholderParams()
     {
         return \array_filter($this->params, function ($value) {
             return $this->isPlaceholderValue($value);
