c0c2b7456381a42c89eb767cf1b9595724c2bd35
Woody Gilk <woody.gilk@gmail.com>
2018-03-29 09:33:29 -0500

    Add support for MySQL found rows
    Fixes #47

---
+++ tests/Query/MySql/SelectTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Latitude\QueryBuilder\Query\MySql;
+
+use Latitude\QueryBuilder\TestCase;
+
+class SelectTest extends TestCase
+{
+    use MySqlEngineSetup;
+
+    public function testCalcFoundRows()
+    {
+        $select = $this->factory
+            ->select()
+            ->calcFoundRows(true)
+            ->from('users')
+            ->limit(10);
+
+        $this->assertSql('SELECT SQL_CALC_FOUND_ROWS * FROM `users` LIMIT 10', $select);
+        $this->assertParams([], $select);
+    }
+}
