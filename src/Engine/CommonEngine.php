89a1fa46299299bccc3e54297922feac62d661cd
Woody Gilk <woody.gilk@gmail.com>
2025-09-22 05:21:01 -0500

    Fix formatting errors

--- src/Engine/CommonEngine.php
+++ src/Engine/CommonEngine.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Latitude\QueryBuilder\Engine;
 
 use function sprintf;
+use function str_replace;
 
 class CommonEngine extends BasicEngine
 {
     public function escapeIdentifier(string $identifier): string
     {
-        return sprintf('"%s"', str_replace( '"', '""', $identifier));
+        return sprintf('"%s"', str_replace('"', '""', $identifier));
     }
 }
